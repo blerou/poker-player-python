ea6ca83e8cdff703db664d0a991808e85ac414a1
blerou <sulik.szabolcs@ustream.tv>
2014-09-20 10:30:13 +0200

    aggressive style

--- player.py
+++ player.py
@@ -1,9 +1,10 @@
 
 class Player:
-    VERSION = "Default Python folding player - test1"
+    VERSION = "vakvarju brutal player"
 
     def betRequest(self, game_state):
-        return 0
+        x = game_state['current_buy_in'] - game_state['players'][game_state['in_action']]['bet']
+        return game_state['pot'] / 2 + x
 
     def showdown(self, game_state):
         pass
